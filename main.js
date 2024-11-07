16a75d73d32b3e5e467bc96a801b93033e26c3cf
milliydev <zohidtojiyev071@gmail.com>
2024-11-07 14:35:06 +0500

    clean code

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 import './style.css'
 import { setupCounter } from './counter.js'
-
+import { Input } from './public/Components/Input/Input.js'
 document.querySelector('#app').innerHTML = `
   
   
 `
 setupCounter(document.querySelector('#counter'))
-
+Input();
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        white: {
+          DEFAULT: "#fff"
+        },
+        grey: {
+          DEFAULT: "#F0F3F7",
+          100: "#B8BBBD"
+        },
+        black: {
+          DEFAULT: "#000",
+          100: "#16191D"
+        }
+      }
+
+      
+    },
   },
   plugins: [],
 }
